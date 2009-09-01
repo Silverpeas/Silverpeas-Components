330b01633180624f6824e39e160f9154e5e30fd9
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-09-01 13:30:42 +0000

    Usage of pdc for user search
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/components/trunk@125 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

---
+++ whitepages/whitepages-war/src/main/java/com/stratelia/silverpeas/pdcPeas/control/PdcSearchUserWrapperSessionController.java
@@ -0,0 +1,134 @@
+package com.stratelia.silverpeas.pdcPeas.control;
+
+import java.util.List;
+
+import com.stratelia.silverpeas.peasCore.AbstractComponentSessionController;
+import com.stratelia.silverpeas.peasCore.ComponentContext;
+import com.stratelia.silverpeas.peasCore.MainSessionController;
+
+/**
+ * A simple wrapper to the userpanel.
+ *
+ * @author Didier Wenzek
+ */
+public class PdcSearchUserWrapperSessionController
+       extends AbstractComponentSessionController
+{
+    /**
+     * Standard Session Controller Constructeur
+     *
+     * @param mainSessionCtrl   The full work session.
+     * @param componentContext  The context of this component session.
+     */
+    public PdcSearchUserWrapperSessionController(
+	    MainSessionController mainSessionCtrl,
+		 ComponentContext componentContext)
+    {
+       super(mainSessionCtrl, componentContext,
+		   "com.stratelia.silverpeas.pdcPeas.multilang.pdcBundle",
+		   "com.stratelia.silverpeas.pdcPeas.settings.pdcPeasIcons");
+    }
+
+	 /**
+	  * Returns the HTML form name whose user element must be set.
+	  */
+	 public String getFormName()
+    {
+       return formName;
+    }
+
+	 /**
+	  * Returns the HTML input where the selected user id must be set.
+	  */
+	 public String getElementId()
+    {
+       return elementId;
+    }
+
+	 /**
+	  * Returns the HTML input where the selected user name must be set.
+	  */
+	 public String getElementName()
+    {
+       return elementName;
+    }
+
+	 /**
+	  * Returns the selected user (if any).
+	  */
+	 public List getSelectedUsers()
+    {
+       return selectedUsers;
+    }
+
+	 /**
+	  * Set the HTML form name whose user element must be set.
+	  */
+	 public void setFormName(String formName)
+    {
+       this.formName = formName;
+    }
+
+	 /**
+	  * Set the HTML input where the selected user id must be set.
+	  */
+	 public void setElementId(String elementId)
+    {
+       this.elementId = elementId;
+    }
+
+	 /**
+	  * Set the HTML input where the selected user name must be set.
+	  */
+	 public void setElementName(String elementName)
+    {
+       this.elementName = elementName;
+    }
+
+	 /**
+	  * Set the selected user (if any).
+	  */
+	 public void setSelectedUserIds(String selectedUserIds)
+    {//parcours de la liste userCardId, userCardId, userCardId ...
+     // Construction d'une liste de GlobalSilverContent à partir de ces Id
+    	
+    }
+
+    /**
+	  * Init the user panel.
+	  */
+	 public void initPdcSearchUser() 
+	 {
+	 }
+	 
+
+    /**
+	  * Reads the selection made with the user panel.
+	  */
+	 public void getUserSelection() 
+	 {
+	 	//get selected objects from PDC
+		selectedUsers = getPdc().getSelectedSilverContents();
+	 }
+
+
+	 /**
+	  * The HTML form name whose user element must be set.
+	  */
+	 private String formName = null;
+
+	 /**
+	  * The HTML input where the selected user id must be set.
+	  */
+	 private String elementId = null;
+
+	 /**
+	  * The HTML input where the selected user name must be set.
+	  */
+	 private String elementName = null;
+
+	 /**
+	  * The selected user (if any).
+	  */
+	 private List selectedUsers = null;
+}
---
+++ whitepages/whitepages-war/src/main/java/com/stratelia/silverpeas/pdcPeas/servlets/PdcSearchUserWrapper.java
@@ -0,0 +1,125 @@
+package com.stratelia.silverpeas.pdcPeas.servlets;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.silverpeas.whitePages.control.CardManager;
+import com.silverpeas.whitePages.model.Card;
+import com.stratelia.silverpeas.contentManager.GlobalSilverContent;
+import com.stratelia.silverpeas.pdcPeas.control.PdcSearchUserWrapperSessionController;
+import com.stratelia.silverpeas.peasCore.ComponentContext;
+import com.stratelia.silverpeas.peasCore.ComponentSessionController;
+import com.stratelia.silverpeas.peasCore.MainSessionController;
+import com.stratelia.silverpeas.peasCore.servlets.ComponentRequestRouter;
+import com.stratelia.silverpeas.silvertrace.SilverTrace;
+
+/**
+ * A simple wrapper for the userpanel. 
+ */
+public class PdcSearchUserWrapper extends ComponentRequestRouter
+{
+    /**
+     * Returns a new session controller
+     */
+    public ComponentSessionController createComponentSessionController(MainSessionController mainSessionCtrl, ComponentContext componentContext)
+    {
+        return new PdcSearchUserWrapperSessionController(
+		     mainSessionCtrl, componentContext);
+    }
+
+    /**
+     * Returns the base name for the session controller of this router.
+     */
+    public String getSessionControlBeanName()
+    {
+        return "pdcSearchUserWrapper";
+    }
+
+    /**
+     * Do the requested function and return the destination url.
+     */
+    public String getDestination(String function,
+	                              ComponentSessionController componentSC,
+											HttpServletRequest request)
+    {
+    	SilverTrace.info("pdcPeas", "PdcSearchUserWrapper.getDestination()", "root.MSG_GEN_PARAM_VALUE", " Function=" + function);
+    	
+        PdcSearchUserWrapperSessionController pdcSearchUserWrapperScc
+		     = (PdcSearchUserWrapperSessionController)componentSC;
+
+        try
+        {
+			if (function.equals("Open"))
+			{
+				pdcSearchUserWrapperScc.setFormName(request.getParameter("formName"));
+				pdcSearchUserWrapperScc.setElementId(request.getParameter("elementId"));
+				pdcSearchUserWrapperScc.setElementName(request.getParameter("elementName"));
+				pdcSearchUserWrapperScc.setSelectedUserIds(request.getParameter("selectedUsers"));
+				pdcSearchUserWrapperScc.initPdcSearchUser(); //selection par défaut d'éléments
+				//return "/RpdcSearch/jsp/ToSearchToSelect?ComponentName=expertLocator&ReturnURL=/RpdcSearchUserWrapper/jsp/Close";
+				return "/RpdcSearch/jsp/ToSearchToSelect?ComponentName=whitePages&ReturnURL=/RpdcSearchUserWrapper/jsp/Close";
+			}
+			else if (function.equals("Close"))
+			{
+				pdcSearchUserWrapperScc.getUserSelection();
+				request.setAttribute("formName", pdcSearchUserWrapperScc.getFormName());
+				request.setAttribute("elementId", pdcSearchUserWrapperScc.getElementId());
+				request.setAttribute("elementName", pdcSearchUserWrapperScc.getElementName());
+				
+				List users = pdcSearchUserWrapperScc.getSelectedUsers();
+				if (users != null)
+				{
+					GlobalSilverContent gsc = null;
+					StringBuffer ids = new StringBuffer("");
+					StringBuffer names = new StringBuffer("");
+					String userCardId = null;
+					String userId = null;
+					CardManager cardM = CardManager.getInstance();
+					Card card = null;
+
+					for (int i=0; i<users.size(); i++) {
+						gsc = (GlobalSilverContent) users.get(i);
+						userCardId = gsc.getId();
+						
+						card = cardM.getCard(new Long(userCardId).longValue());
+						userId = card.getUserId(); 
+						
+						ids.append(userCardId);
+						ids.append("-");
+						ids.append(userId);
+						ids.append(",");
+						
+						names.append(gsc.getName());
+						names.append(",");
+					} 
+					
+					if (! ids.toString().equals("")) {
+						ids = ids.deleteCharAt(ids.length() - 1);
+						names = names.deleteCharAt(names.length() - 1);
+						
+					}
+					
+					request.setAttribute("userIds", ids.toString());
+					request.setAttribute("userNames", names.toString());
+				}					
+					
+				else
+				{
+					request.setAttribute("userIds", "");
+					request.setAttribute("userNames", "");
+				}
+				return "/pdcPeas/jsp/closeWrapper.jsp";
+			}
+			else
+			{
+				return "/RpdcSearch/jsp/"+function;
+			}
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("javax.servlet.jsp.jspException", e);
+            return "/admin/jsp/errorpageMain.jsp";
+        }
+    }
+}
