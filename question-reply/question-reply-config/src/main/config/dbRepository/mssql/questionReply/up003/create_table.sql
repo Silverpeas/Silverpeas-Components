ca37f26d69bdb2d188ebce2132aea993bde99437
Nicolas Eysseric <nicolas.eysseric@silverpeas.com>
2009-11-20 14:15:31 +0000

    fixes bug #47
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/components/trunk@316 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

---
+++ question-reply/question-reply-config/src/main/config/dbRepository/mssql/questionReply/up003/create_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE SC_QuestionReply_Question
+ALTER COLUMN content varchar(2000) NULL
+;
+
+ALTER TABLE SC_QuestionReply_Reply
+ALTER COLUMN content varchar(2000) NULL
+;
---
+++ question-reply/question-reply-config/src/main/config/dbRepository/oracle/questionReply/up003/create_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE SC_QuestionReply_Question
+MODIFY content NULL
+;
+
+ALTER TABLE SC_QuestionReply_Reply
+MODIFY content NULL
+;
