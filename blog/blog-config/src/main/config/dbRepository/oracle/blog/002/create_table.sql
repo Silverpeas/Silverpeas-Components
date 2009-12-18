7bd2ec50e0525b89c3f254bd83f5ac3d686363cd
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-12-18 15:05:25 +0000

    Incorrect oracle script
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/components/trunk@404 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- blog/blog-config/src/main/config/dbRepository/oracle/blog/002/create_table.sql
+++ blog/blog-config/src/main/config/dbRepository/oracle/blog/002/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE SC_Blog_Post 
 (
-	pubId		int		NOT NULL ,
-	dateEvent	char(13)	NOT NULL DEFAULT('0000000000000') ,
-	instanceId	varchar (50)	NOT NULL
+  pubId int NOT NULL,
+  dateEvent char(13) DEFAULT '0000000000000' NOT NULL,
+  instanceId varchar (50) NOT NULL
 );
