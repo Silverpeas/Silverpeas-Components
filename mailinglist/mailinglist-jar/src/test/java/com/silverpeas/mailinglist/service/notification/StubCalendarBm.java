73372639b99a86f6245768062b3f390d2487ec62
Sebastien Rochet <sebastien.rochet@oosphere.com>
2010-07-26 13:59:18 +0000

    Fixing Bug #829
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/components/trunk@1966 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- mailinglist/mailinglist-jar/src/test/java/com/silverpeas/mailinglist/service/notification/StubCalendarBm.java
+++ mailinglist/mailinglist-jar/src/test/java/com/silverpeas/mailinglist/service/notification/StubCalendarBm.java
@@ -35,7 +35,6 @@ import javax.ejb.Handle;
 import javax.ejb.RemoveException;
 
 import com.stratelia.webactiv.calendar.control.CalendarBm;
-import com.stratelia.webactiv.calendar.control.CalendarException;
 import com.stratelia.webactiv.calendar.model.Attendee;
 import com.stratelia.webactiv.calendar.model.Category;
 import com.stratelia.webactiv.calendar.model.HolidayDetail;
@@ -54,8 +53,8 @@ public class StubCalendarBm implements CalendarBm {
 
   }
 
-  public String addJournal(JournalHeader journal) throws RemoteException,
-      CalendarException {
+  public String addJournal(JournalHeader journal) throws RemoteException
+  {
     // TODO Auto-generated method stub
     return null;
   }
@@ -72,8 +71,8 @@ public class StubCalendarBm implements CalendarBm {
 
   }
 
-  public String addToDo(ToDoHeader todo) throws RemoteException,
-      CalendarException {
+  public String addToDo(ToDoHeader todo) throws RemoteException
+      {
     return "" + todo.hashCode();
   }
 
@@ -292,14 +291,14 @@ public class StubCalendarBm implements CalendarBm {
 
   }
 
-  public void updateJournal(JournalHeader journal) throws RemoteException,
-      CalendarException {
+  public void updateJournal(JournalHeader journal) throws RemoteException
+  {
     // TODO Auto-generated method stub
 
   }
 
-  public void updateToDo(ToDoHeader todo) throws RemoteException,
-      CalendarException {
+  public void updateToDo(ToDoHeader todo) throws RemoteException
+  {
     // TODO Auto-generated method stub
 
   }
