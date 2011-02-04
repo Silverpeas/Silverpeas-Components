d325243ca2b918c1f67ea858635761fdd6c8f471
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2011-02-04 13:36:36 +0000

    Feature #1442 Now the authorization of the user to access the asked almanach is checked
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/components/trunk@3483 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- almanach/almanach-war/src/main/java/com/stratelia/webactiv/almanach/servlets/AlmanachICSProducer.java
+++ almanach/almanach-war/src/main/java/com/stratelia/webactiv/almanach/servlets/AlmanachICSProducer.java
@@ -33,6 +33,7 @@ import com.stratelia.webactiv.almanach.control.ejb.AlmanachException;
 import com.stratelia.webactiv.almanach.model.EventDetail;
 import com.stratelia.webactiv.almanach.model.EventPK;
 import com.stratelia.webactiv.beans.admin.AdminController;
+import com.stratelia.webactiv.beans.admin.OrganizationController;
 import com.stratelia.webactiv.beans.admin.UserFull;
 import com.stratelia.webactiv.util.EJBUtilitaire;
 import com.stratelia.webactiv.util.JNDINames;
