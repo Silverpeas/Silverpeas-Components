d66fd5b2af36d258513a4a2f202e85e75cd2e17f
Stephane PERRY <stephane.perry@gmail.com>
2011-09-07 15:21:27 +0200

    DB Scripts for data v003 (company concept added for contacts) - corrections
    Syntax correction in scripts

--- yellowpages/yellowpages-config/src/main/config/dbRepository/mssql/yellowpages/003/create_constraint.sql
+++ yellowpages/yellowpages-config/src/main/config/dbRepository/mssql/yellowpages/003/create_constraint.sql
@@ -2,32 +2,32 @@ ALTER TABLE SC_Contact_GroupFather
 ADD CONSTRAINT PK_SC_Contact_GroupFather PRIMARY KEY
 	(
 		groupId, fatherId, instanceId
+	)
+;
+	
+ALTER TABLE SC_Contact_Company ADD
+	 CONSTRAINT PK_Contact_Company PRIMARY KEY 
+	(
+		companyId
 	)   
 ;
 
-ALTER TABLE SC_Contact_Company_Rel WITH NOCHECK ADD
-	 CONSTRAINT PK_Contact_Company_Rel PRIMARY KEY CLUSTERED 
+ALTER TABLE SC_Contact_Company_Rel ADD
+	 CONSTRAINT PK_Contact_Company_Rel PRIMARY KEY 
 	(
 		contactId,
 		companyId
 	)   
 ;
 
-ALTER TABLE SC_Contact_Company_Rel WITH NOCHECK ADD
+ALTER TABLE SC_Contact_Company_Rel ADD
 	CONSTRAINT FK_Contact_Company_Rel_FKContact FOREIGN KEY (contactId)
     REFERENCES SB_Contact_Contact (contactId) MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION
 ;
 
-ALTER TABLE SC_Contact_Company_Rel WITH NOCHECK ADD
+ALTER TABLE SC_Contact_Company_Rel ADD
 	CONSTRAINT FK_Contact_Company_Rel_FKCompany FOREIGN KEY (companyId)
     REFERENCES SC_Contact_Company (companyId) MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION
-;
-
-ALTER TABLE SC_Contact_Company WITH NOCHECK ADD
-	 CONSTRAINT PK_Contact_Company PRIMARY KEY CLUSTERED 
-	(
-		companyId
-	)   
 ;
