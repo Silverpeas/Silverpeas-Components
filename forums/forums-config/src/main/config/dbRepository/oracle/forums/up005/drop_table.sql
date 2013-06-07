e0c643cff043dbb39ddd0842e7f85134b296c84c
Yohann Chastagnier <yohann.chastagnier@silverpeas.com>
2013-06-07 15:52:01 +0200

    Feature #3178 - upgrading forum subscriptions - wiring specific message subscription services to the common subscription services

---
+++ forums/forums-config/src/main/config/dbRepository/oracle/forums/up005/drop_table.sql
@@ -0,0 +1,21 @@
+-- Copy message subscriptions of forum into new structure
+INSERT INTO subscribe
+(subscriberId, subscriberType, subscriptionMethod, resourceId, resourceType, space, instanceId, creatorId, creationDate)
+  SELECT
+    sfs.userid,
+    'USER',
+    'SELF_CREATION',
+    sfs.messageid,
+    'FORUM_MESSAGE',
+    '-',
+    sff.instanceid,
+    sfs.userid,
+    to_date('1970-01-01', 'YYYY-MM-DD')
+  FROM sc_forums_subscription sfs
+    JOIN sc_forums_message sfm
+      ON sfm.messageid = cast(sfs.messageid AS NUMERIC)
+    JOIN sc_forums_forum sff
+      ON sff.forumid = sfm.forumid;
+
+-- Drop unnecessary tables of forum component
+DROP TABLE sc_forums_subscription;
---
+++ forums/forums-config/src/main/config/dbRepository/postgres/forums/006/drop_table.sql
@@ -0,0 +1,4 @@
+DROP TABLE SC_Forums_Forum;
+DROP TABLE SC_Forums_Message;
+DROP TABLE SC_Forums_Rights;
+DROP TABLE SC_Forums_HistoryUser;
