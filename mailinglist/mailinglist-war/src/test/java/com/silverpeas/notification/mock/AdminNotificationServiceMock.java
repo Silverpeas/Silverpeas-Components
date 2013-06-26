0718e27f82cc29237a5fe95d16a01f8a9642cfc6
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2013-06-26 18:28:51 +0200

    switching kmelia :  Adding listener to component configuration change listening for modification of the version control parameter.

--- mailinglist/mailinglist-war/src/test/java/com/silverpeas/notification/mock/AdminNotificationServiceMock.java
+++ mailinglist/mailinglist-war/src/test/java/com/silverpeas/notification/mock/AdminNotificationServiceMock.java
@@ -3,13 +3,18 @@ package com.silverpeas.notification.mock;
 import javax.inject.Named;
 
 import com.silverpeas.admin.notification.AdminNotificationService;
+import com.silverpeas.admin.notification.ComponentJsonPatch;
 
 @Named("adminNotificationService")
 public class AdminNotificationServiceMock implements AdminNotificationService {
 
   @Override
   public void notifyOnDeletionOf(String spaceId, String userId) {
-    
+  }
+
+  @Override
+  public void notifyOfComponentConfigurationChange(String componentId, String userId,
+      ComponentJsonPatch changes) {
   }
 
 }
