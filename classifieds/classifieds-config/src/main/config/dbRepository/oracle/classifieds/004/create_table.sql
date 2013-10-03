4021ef725b51e4d8eb029ac7bf90028d6d28a2c8
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-10-03 16:50:09 +0200

    bug #4926 :  Some users had a problem with the database migration of classifieds up to 003 with MSSQL. In order the migration works, they had have to split the migration step 003 into two separates ones: 003 (that updates the SQL tables for the classifieds component) and 004 (to migrate the classifieds data into the JCR). This change cannot be reported into the classifieds source, so, for they can profit of a possible next database migration of classifieds, a virtual migration step is introduced: 004/up003; with this virtual step, the next concrete database migration of classifieds will be step 005.

---
+++ classifieds/classifieds-config/src/main/config/dbRepository/oracle/classifieds/004/create_table.sql
@@ -0,0 +1,24 @@
+CREATE TABLE SC_Classifieds_Classifieds
+(
+	ClassifiedId	int		NOT NULL,
+	instanceId	varchar(50)	NOT NULL,
+	title		varchar(255)	NOT NULL,
+	description	varchar(4000)	NOT NULL,
+	price		int 			NULL,
+	creatorId	varchar(50)	NOT NULL,
+	creationDate	varchar(13)	NOT NULL,
+	updateDate	varchar(13)	NULL,
+	status		varchar(50)	NOT NULL,
+	validatorId	varchar(50)	NULL,
+	validateDate	varchar(13)	NULL
+) 
+;
+
+CREATE TABLE SC_Classifieds_Subscribes
+(
+	SubscribeId	int		NOT NULL,
+	userId		varchar(50)	NOT NULL,
+	instanceId	varchar(50)	NOT NULL,
+	field1		varchar(100)	NOT NULL,
+	field2		varchar(100)	NOT NULL
+);
