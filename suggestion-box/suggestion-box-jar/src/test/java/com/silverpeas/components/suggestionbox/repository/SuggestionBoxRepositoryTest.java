ee83d8848e3e79e5501d84267bb3239b3f5dc46b
Yohann Chastagnier <yohann.chastagnier@silverpeas.com>
2014-03-11 18:43:24 +0100

    Feature #907 - adding unit tests around the JPA entities and their JPA repositories.

---
+++ suggestion-box/suggestion-box-jar/src/test/java/com/silverpeas/components/suggestionbox/repository/SuggestionBoxRepositoryTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2000 - 2014 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception. You should have recieved a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.silverpeas.components.suggestionbox.repository;
+
+import com.silverpeas.components.suggestionbox.model.Suggestion;
+import com.silverpeas.components.suggestionbox.model.SuggestionBox;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.ReplacementDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * User: Yohann Chastagnier
+ * Date: 11/03/14
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    locations = {"/spring-suggestion-box.xml", "/spring-suggestion-box-embedded-datasource.xml"})
+public class SuggestionBoxRepositoryTest {
+
+  private final static String SUGGESTION_BOX_ID_1 = "suggestion-box_1";
+
+  @Inject
+  private SuggestionBoxRepository suggestionBoxRepository;
+
+  @Inject
+  private SuggestionRepository suggestionRepository;
+
+  @Inject
+  @Named("jpaDataSource")
+  private DataSource dataSource;
+
+  private static ReplacementDataSet dataSet;
+
+  @BeforeClass
+  public static void prepareDataSet() throws Exception {
+    dataSet = new ReplacementDataSet(new FlatXmlDataSetBuilder().build(
+        SuggestionBoxRepositoryTest.class.getClassLoader().getResourceAsStream(
+            "com/silverpeas/components/suggestionbox/suggestion-box-dataset.xml")));
+    dataSet.addReplacementObject("[NULL]", null);
+  }
+
+  /**
+   * Complete reload of Spring context is explicitly implemented here by methods behind {@link
+   * org.junit.Before} and {@link org.junit.After} annotations.
+   * @throws Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    final IDatabaseConnection myConnection = new DatabaseConnection(dataSource.getConnection());
+    DatabaseOperation.CLEAN_INSERT.execute(myConnection, dataSet);
+  }
+
+  /**
+   * Deletion of a suggestion box, at a repository level, must delete all associated suggestions.
+   */
+  @Test
+  @Transactional
+  public void deleteSuggestionBox() {
+    SuggestionBox existentSuggestionBox = suggestionBoxRepository.getById(SUGGESTION_BOX_ID_1);
+    assertThat(existentSuggestionBox, notNullValue());
+    assertThat(existentSuggestionBox.getId(), is(SUGGESTION_BOX_ID_1));
+
+    List<Suggestion> associatedSuggestions =
+        suggestionRepository.listBySuggestionBox(existentSuggestionBox);
+    assertThat(associatedSuggestions, hasSize(1));
+
+    // The suggestion box deletion
+    suggestionBoxRepository.delete(existentSuggestionBox);
+
+    // Verifications
+    assertThat(suggestionBoxRepository.getById(SUGGESTION_BOX_ID_1), nullValue());
+    assertThat(suggestionRepository.listBySuggestionBox(existentSuggestionBox), hasSize(0));
+  }
+}
---
+++ suggestion-box/suggestion-box-jar/src/test/resources/com/silverpeas/components/suggestionbox/create-database.sql
@@ -0,0 +1,27 @@
+/* Table */
+CREATE TABLE sc_suggestion_box (
+  id                  VARCHAR(40) NOT NULL,
+  componentInstanceId VARCHAR(30) NOT NULL,
+  createDate          TIMESTAMP   NOT NULL,
+  createdBy           VARCHAR(40) NOT NULL,
+  lastUpdateDate      TIMESTAMP,
+  lastUpdatedBy       VARCHAR(40),
+  version             INT8        NOT NULL
+);
+
+/* Table constraints */
+ALTER TABLE sc_suggestion_box ADD CONSTRAINT const_sc_suggestion_box_pk PRIMARY KEY (id);
+
+/* Table */
+CREATE TABLE sc_suggestion (
+  id              VARCHAR(40) NOT NULL,
+  suggestionBoxId VARCHAR(40) NOT NULL,
+  createDate      TIMESTAMP   NOT NULL,
+  createdBy       VARCHAR(40) NOT NULL,
+  lastUpdateDate  TIMESTAMP,
+  lastUpdatedBy   VARCHAR(40),
+  version         INT8        NOT NULL
+);
+
+/* Table constraints */
+ALTER TABLE sc_suggestion ADD CONSTRAINT const_sc_suggestion_pk PRIMARY KEY (id);
