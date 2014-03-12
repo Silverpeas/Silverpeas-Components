455cef2f70ccaaa90aa435010d7025ca44f7f483
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2014-03-12 11:36:00 +0100

    feature #907 sprint1 :   When the application Suggestion Box is instanciated, a new SuggestionBox instance is then created and persisted. Unit test is added to check the instanciator creates a new SuggestionBox instance for each application instanciation

--- suggestion-box/suggestion-box-jar/src/main/java/com/silverpeas/components/suggestionbox/SuggestionBoxInstanciator.java
+++ suggestion-box/suggestion-box-jar/src/main/java/com/silverpeas/components/suggestionbox/SuggestionBoxInstanciator.java
@@ -9,22 +9,25 @@
  * As a special exception to the terms and conditions of version 3.0 of
  * the GPL, you may redistribute this Program in connection with Free/Libre
  * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
+ * FLOSS exception. You should have received a copy of the text describing
  * the FLOSS exception, and it is also available here:
  * "http://www.silverpeas.com/legal/licensing"
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.silverpeas.components.suggestionbox;
 
 import com.silverpeas.admin.components.ComponentsInstanciatorIntf;
 import com.silverpeas.admin.components.InstanciationException;
+import com.silverpeas.components.suggestionbox.model.SuggestionBox;
+import com.silverpeas.components.suggestionbox.model.SuggestionBoxService;
+import com.silverpeas.components.suggestionbox.model.SuggestionBoxServiceFactory;
 import com.stratelia.silverpeas.silvertrace.SilverTrace;
 
 import java.sql.Connection;
@@ -42,7 +45,11 @@ public class SuggestionBoxInstanciator implements ComponentsInstanciatorIntf {
         .info("suggestion-box", "SuggestionBoxInstanciator.create()", "root.MSG_GEN_ENTER_METHOD",
             "space = " + spaceId + ", componentId = " + componentId + ", userId =" + userId);
 
-    //insert your code here !
+    // the title of the suggestion box is the one of the spawned component instance
+    SuggestionBox newBox = new SuggestionBox(componentId, "");
+    newBox.setCreatedBy(userId);
+    SuggestionBoxService suggestionBoxService = SuggestionBoxServiceFactory.getServiceInstance();
+    suggestionBoxService.saveSuggestionBox(newBox);
 
     SilverTrace
         .info("suggestion-box", "SuggestionBoxInstanciator.create()", "root.MSG_GEN_EXIT_METHOD");
---
+++ suggestion-box/suggestion-box-jar/src/test/java/com/silverpeas/components/suggestionbox/mock/SuggestionBoxRepositoryMockWrapper.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2000-2014 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Writer Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have recieved a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "http://www.silverpeas.org/legal/licensing"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.silverpeas.components.suggestionbox.mock;
+
+import com.silverpeas.components.suggestionbox.model.SuggestionBox;
+import com.silverpeas.components.suggestionbox.repository.SuggestionBoxRepository;
+import org.silverpeas.persistence.model.identifier.UuidIdentifier;
+import org.silverpeas.persistence.repository.OperationContext;
+
+import static org.mockito.Mockito.mock;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Named;
+
+/**
+ * A wrapper of the SuggestionBoxRepository mock for testing purpose and to be managed by the IoC
+ * container.
+ * @author mmoquillon
+ */
+@Named("suggestionBoxRepository")
+public class SuggestionBoxRepositoryMockWrapper extends SuggestionBoxRepository {
+
+  private SuggestionBoxRepository mock = mock(SuggestionBoxRepository.class);
+
+  public SuggestionBoxRepository getMock() {
+    return mock;
+  }
+
+  @Override
+  public List<SuggestionBox> getAll() {
+    return mock.getAll();
+  }
+
+  @Override
+  public SuggestionBox getById(String id) {
+    return mock.getById(id);
+  }
+
+  @Override
+  public List<SuggestionBox> getById(String... ids) {
+    return mock.getById(ids);
+  }
+
+  @Override
+  public List<SuggestionBox> getById(Collection<String> ids) {
+    return mock.getById(ids);
+  }
+
+  @Override
+  public SuggestionBox getByIdentifier(UuidIdentifier id) {
+    return mock.getByIdentifier(id);
+  }
+
+  @Override
+  public List<SuggestionBox> getByIdentifier(UuidIdentifier... ids) {
+    return mock.getByIdentifier(ids);
+  }
+
+  @Override
+  public List<SuggestionBox> getByIdentifier(Collection<UuidIdentifier> ids) {
+    return mock.getByIdentifier(ids);
+  }
+
+  @Override
+  public SuggestionBox save(OperationContext context, SuggestionBox entity) {
+    return mock.save(context, entity);
+  }
+
+  @Override
+  public List<SuggestionBox> save(OperationContext context, SuggestionBox... entities) {
+    return mock.save(context, entities);
+  }
+
+  @Override
+  public List<SuggestionBox> save(OperationContext context, List<SuggestionBox> entities) {
+    return mock.save(context, entities);
+  }
+
+  @Override
+  public void delete(SuggestionBox... entity) {
+    mock.delete(entity);
+  }
+
+  @Override
+  public void delete(List<SuggestionBox> entities) {
+    mock.delete(entities);
+  }
+
+  @Override
+  public long deleteById(String... ids) {
+    return mock.deleteById(ids);
+  }
+
+  @Override
+  public long deleteById(Collection<String> ids) {
+    return mock.deleteById(ids);
+  }
+
+  @Override
+  public long deleteByIdentifier(UuidIdentifier... ids) {
+    return mock.deleteByIdentifier(ids);
+  }
+
+  @Override
+  public long deleteByIdentifier(Collection<UuidIdentifier> ids) {
+    return mock.deleteByIdentifier(ids);
+  }
+
+
+}
