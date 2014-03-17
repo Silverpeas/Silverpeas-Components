8125716126168dc4324ffacf980c222d4f59898d
Yohann Chastagnier <yohann.chastagnier@silverpeas.com>
2014-03-17 09:32:27 +0100

    Feature #907 : refactoring request router mechanism.

--- suggestion-box/suggestion-box-war/src/main/java/org/silverpeas/components/suggestionbox/servlets/SuggestionBoxRequestRouter.java
+++ suggestion-box/suggestion-box-war/src/main/java/org/silverpeas/components/suggestionbox/servlets/SuggestionBoxRequestRouter.java
@@ -26,21 +26,22 @@ package org.silverpeas.components.suggestionbox.servlets;
 import com.stratelia.silverpeas.peasCore.ComponentContext;
 import com.stratelia.silverpeas.peasCore.MainSessionController;
 import com.stratelia.silverpeas.peasCore.URLManager;
-import com.stratelia.silverpeas.peasCore.servlets.ComponentRequestRouter;
-import com.stratelia.silverpeas.silvertrace.SilverTrace;
+import com.stratelia.silverpeas.peasCore.servlets.Navigation;
+import com.stratelia.silverpeas.peasCore.servlets.WebComponentRequestRouter;
+import com.stratelia.silverpeas.peasCore.servlets.annotation.Homepage;
+import com.stratelia.silverpeas.peasCore.servlets.annotation.LowestRoleAccess;
 import com.stratelia.webactiv.SilverpeasRole;
 import org.apache.commons.lang3.CharEncoding;
 import org.silverpeas.components.suggestionbox.control.SuggestionBoxSessionController;
-import org.silverpeas.components.suggestionbox.model.SuggestionBox;
-import org.silverpeas.servlet.HttpRequest;
 import org.silverpeas.wysiwyg.control.WysiwygController;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public class SuggestionBoxRequestRouter
-    extends ComponentRequestRouter<SuggestionBoxSessionController> {
+    extends WebComponentRequestRouter<SuggestionBoxSessionController, SuggestionBoxWebContext> {
   private static final long serialVersionUID = -7378638602035981580L;
 
   /**
@@ -66,102 +67,59 @@ public class SuggestionBoxRequestRouter
     return new SuggestionBoxSessionController(mainSessionCtrl, componentContext);
   }
 
-  /**
-   * This method has to be implemented by the component request rooter
-   * it has to compute a destination page
-   * @param function The entering request function (ex : "Main.jsp")
-   * @param suggestionBoxSC The component Session Control, build and initialised.
-   * @return The complete destination URL for a forward (ex : "/almanach/jsp/almanach
-   * .jsp?flag=user")
-   */
   @Override
-  public String getDestination(String function, SuggestionBoxSessionController suggestionBoxSC,
-      HttpRequest request) {
-    String destination;
-    SilverTrace.info("suggestionBox", "SuggestionBoxRequestRouter.getDestination()",
-        "root.MSG_GEN_PARAM_VALUE",
-        "User=" + suggestionBoxSC.getUserId() + " Function=" + function);
-
-    // Greater user role
-    SilverpeasRole greaterUserRole =
-        SilverpeasRole.getGreaterFrom(SilverpeasRole.from(suggestionBoxSC.getUserRoles()));
-
-    // Suggestion box is reloaded at each request to ensure that the entity is always the last
-    // persisted one.
-    SuggestionBox suggestionBox =
-        SuggestionBox.getByComponentInstanceId(suggestionBoxSC.getComponentId());
-    request.setAttribute("suggestionBox", suggestionBox);
-    request.setAttribute("greaterUserRole", greaterUserRole);
-
-    try {
-      // External destinations
-      if (function.equals("edito/modify") &&
-          greaterUserRole.isGreaterThanOrEquals(SilverpeasRole.admin)) {
-        return modifyEdito(request, suggestionBoxSC, suggestionBox);
-      }
-
-      // Internal JSP destinations
-      if (function.equals("Main")) {
-        destination = home(request, suggestionBoxSC, suggestionBox);
-      } else {
-        destination = home(request, suggestionBoxSC, suggestionBox);
-      }
-      destination = "/suggestionBox/jsp/" + destination;
-    } catch (Exception e) {
-      request.setAttribute("javax.servlet.jsp.jspException", e);
-      destination = "/admin/jsp/errorpageMain.jsp";
-    }
-
-    SilverTrace.info("suggestionBox", "SuggestionBoxRequestRouter.getDestination()",
-        "root.MSG_GEN_PARAM_VALUE", "Destination=" + destination);
-    return destination;
+  protected void commonContextInitialization(final SuggestionBoxWebContext context) {
+    super.commonContextInitialization(context);
+    context.getRequest().setAttribute("suggestionBox", context.getSuggestionBox());
   }
 
   /**
    * Perform homepage
-   * @param request
-   * @param componentSC
-   * @param suggestionBox
+   * @param context
    * @return destination
    */
-  private String home(HttpServletRequest request, SuggestionBoxSessionController componentSC,
-      final SuggestionBox suggestionBox) {
+  @GET
+  @Homepage
+  public Navigation home(SuggestionBoxWebContext context) {
 
     // Edito
-    if (WysiwygController
-        .haveGotWysiwyg(componentSC.getComponentId(), suggestionBox.getId(), null)) {
-      request.setAttribute("isEdito", true);
-    }
+    setIsEdito(context);
 
     // Destination
-    return "suggestionBox.jsp";
+    return context.navigateToInternalJsp("suggestionBox.jsp");
+  }
+
+  @GET
+  @Path("edito/modify")
+  @LowestRoleAccess(SilverpeasRole.admin)
+  public Navigation modifyEdito(SuggestionBoxWebContext context)
+      throws UnsupportedEncodingException {
+    context.getRequest().setAttribute("SpaceId", context.getSpaceId());
+    context.getRequest()
+        .setAttribute("SpaceName", URLEncoder.encode(context.getSpaceLabel(), CharEncoding.UTF_8));
+    context.getRequest().setAttribute("ComponentId", context.getComponentInstanceId());
+    context.getRequest().setAttribute("ComponentName",
+        URLEncoder.encode(context.getComponentInstanceLabel(), CharEncoding.UTF_8));
+    context.getRequest().setAttribute("ObjectId", context.getSuggestionBox().getId());
+    context.getRequest().setAttribute("Language", null);
+    context.getRequest().setAttribute("ReturnUrl", URLManager.getApplicationURL() +
+        URLManager.getURL("suggestionBox", "useless", context.getComponentInstanceId()) +
+        "fromWysiwyg");
+    context.getRequest().setAttribute("UserId", context.getUser().getId());
+    context.getRequest().setAttribute("IndexIt", "false");
+    // Destination
+    return context.navigateTo("/wysiwyg/jsp/htmlEditor.jsp");
   }
 
   /**
-   * Handles the navigation to the edition of the edito.
-   * @param request
-   * @param suggestionBoxSC
-   * @param suggestionBox
-   * @return
-   * @throws UnsupportedEncodingException
+   * Sets into request attributes the isEdito constants
+   * @param context
    */
-  private String modifyEdito(HttpServletRequest request,
-      SuggestionBoxSessionController suggestionBoxSC, SuggestionBox suggestionBox)
-      throws UnsupportedEncodingException {
-    request.setAttribute("SpaceId", suggestionBoxSC.getSpaceId());
-    request.setAttribute("SpaceName",
-        URLEncoder.encode(suggestionBoxSC.getSpaceLabel(), CharEncoding.UTF_8));
-    request.setAttribute("ComponentId", suggestionBoxSC.getComponentId());
-    request.setAttribute("ComponentName",
-        URLEncoder.encode(suggestionBoxSC.getComponentLabel(), CharEncoding.UTF_8));
-    request.setAttribute("ObjectId", suggestionBox.getId());
-    request.setAttribute("Language", null);
-    request.setAttribute("ReturnUrl", URLManager.getApplicationURL() +
-        URLManager.getURL("suggestionBox", "useless", suggestionBoxSC.getComponentId()) +
-        "fromWysiwyg");
-    request.setAttribute("UserId", suggestionBoxSC.getUserId());
-    request.setAttribute("IndexIt", "false");
-    // Destination
-    return "/wysiwyg/jsp/htmlEditor.jsp";
+  private void setIsEdito(SuggestionBoxWebContext context) {
+    if (WysiwygController
+        .haveGotWysiwyg(context.getComponentInstanceId(), context.getSuggestionBox().getId(),
+            null)) {
+      context.getRequest().setAttribute("isEdito", true);
+    }
   }
 }
