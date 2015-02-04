91fe4a9f0b2bd910dff4df3d9ea20a4123fc3a55
Xavier MICHAU <xavier.michau@oosphere.com>
2015-02-04 10:12:29 +0100

    Feature #6237 Recontact participants of a meeting

---
+++ scheduleevent/scheduleevent-war/src/main/java/com/silverpeas/scheduleevent/servlets/handlers/ScheduleEventCallAgainRequestHandler.java
@@ -0,0 +1,32 @@
+package com.silverpeas.scheduleevent.servlets.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.silverpeas.util.NotifierUtil;
+
+import com.silverpeas.scheduleevent.control.ScheduleEventSessionController;
+
+public class ScheduleEventCallAgainRequestHandler implements ScheduleEventRequestHandler {
+
+  private String jspDestination;
+
+  private ScheduleEventDetailRequestHandler detailHandler;
+
+  public ScheduleEventCallAgainRequestHandler(String jspDestination) {
+    this.jspDestination = jspDestination;
+  }
+
+  @Override
+  public String getDestination(String function, ScheduleEventSessionController scheduleeventSC,
+      HttpServletRequest request) throws Exception {
+    String message = request.getParameter("message_content");
+    scheduleeventSC.sendCallAgainNotification("callagain", message);
+    NotifierUtil.addSuccess(scheduleeventSC.getString("scheduleevent.callagain.ok"));
+    return detailHandler.getDestination(function, scheduleeventSC, request);
+  }
+
+  public void setDetailHandler(ScheduleEventDetailRequestHandler detailHandler) {
+    this.detailHandler = detailHandler;
+  }
+
+}
