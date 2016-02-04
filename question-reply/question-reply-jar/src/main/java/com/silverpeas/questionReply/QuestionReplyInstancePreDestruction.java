69bffc27d79d68d7a6c39600551a589116fd5372
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2016-02-04 10:32:49 +0100

    Delete QuestionReplyInstanciator and replace its delete method by the QuestionReplyInstancePreDestruction class that implements the ComponentInstancePreDestruction interface in order to be invoked when a QuestionReply instance is being deleted

---
+++ question-reply/question-reply-jar/src/main/java/com/silverpeas/questionReply/QuestionReplyInstancePreDestruction.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2000 - 2015 Silverpeas
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * <p>
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have received a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.silverpeas.questionReply;
+
+import com.silverpeas.admin.components.ComponentInstancePreDestruction;
+import org.silverpeas.util.DBUtil;
+
+import javax.inject.Named;
+import javax.transaction.Transactional;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+/**
+ * Deletes all the questions and their answers that are managed by the QuestionReply instance that
+ * is being deleted.
+ * @author mmoquillon
+ */
+@Named
+public class QuestionReplyInstancePreDestruction implements ComponentInstancePreDestruction {
+
+  private static final String RECIPIENT_DELETION =
+      "DELETE FROM SC_QuestionReply_Recipient WHERE questionId in (SELECT id FROM " +
+          "SC_QuestionReply_Question WHERE instanceId = ?)";
+  private static final String ANSWER_DELETION =
+      "DELETE FROM SC_QuestionReply_Reply WHERE questionId in (SELECT id FROM " +
+          "SC_QuestionReply_Question WHERE instanceId = ?)";
+  private static final String QUESTION_DELETION =
+      "DELETE FROM SC_QuestionReply_Question WHERE instanceId = ?";
+
+  /**
+   * Performs pre destruction tasks in the behalf of the specified component instance.
+   * @param componentInstanceId the unique identifier of the component instance.
+   */
+  @Transactional
+  @Override
+  public void preDestroy(final String componentInstanceId) {
+    try (Connection connection = DBUtil.openConnection()) {
+      for (String sql : new String[]{RECIPIENT_DELETION, ANSWER_DELETION, QUESTION_DELETION}) {
+        try (PreparedStatement deletion = connection.prepareStatement(sql)) {
+          deletion.setString(1, componentInstanceId);
+          deletion.execute();
+        }
+      }
+    } catch(SQLException e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
+  }
+}
