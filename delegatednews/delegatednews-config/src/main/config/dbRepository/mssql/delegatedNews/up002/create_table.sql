9725899e398e4da35b58e73bf35efb7f21c1f345
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2016-02-29 09:58:31 +0100

    Refactor the name of both the directories and the artifact ids of the DelegatedNews subprojects

--- delegatednews/delegatednews-config/src/main/config/dbRepository/mssql/delegatedNews/up002/create_table.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE sc_delegatednews_news 
-ADD newsOrder int not null DEFAULT (0)
-;
---
+++ delegatednews/delegatednews-configuration/src/main/config/dbRepository/oracle/delegatedNews/002/create_constraint.sql
@@ -0,0 +1,4 @@
+
+alter table sc_delegatednews_news
+        add constraint pk_delegatednews_news
+        primary key (pubId);
