2f1995e98fdc01efbc2e467b550d996b3e9ba53f
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2016-03-22 14:53:54 +0100

    Fix an integration test in forums

--- forums/forums-library/src/integration-test/java/org/silverpeas/components/forums/ForumsInstancePreDestructionIntegrationTest.java
+++ forums/forums-library/src/integration-test/java/org/silverpeas/components/forums/ForumsInstancePreDestructionIntegrationTest.java
@@ -43,7 +43,7 @@ import org.silverpeas.attachment.model.SimpleDocumentPK;
 import org.silverpeas.attachment.model.UnlockContext;
 import org.silverpeas.attachment.util.SimpleDocumentList;
 import org.silverpeas.components.forums.test.WarBuilder4Forums;
-import org.silverpeas.core.search.indexEngine.model.FullIndexEntry;
+import org.silverpeas.core.index.indexing.model.FullIndexEntry;
 import org.silverpeas.test.rule.DbSetupRule;
 import org.silverpeas.util.ForeignPK;
 import org.silverpeas.util.WAPrimaryKey;
