1d51c2046190aec85f3e87c00e016ff4bdd5dbe7
NicolasEysseric <nicolas.eysseric@silverpeas.com>
2018-03-01 16:51:47 +0100

    closing feature #8238 - increasing some columns length

---
+++ formsOnline/formsOnline-configuration/src/main/config/migrations/db/mssql/formsOnline/up001/alter_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE SC_FormsOnline_Forms ALTER COLUMN name varchar(200) NOT NULL;
+ALTER TABLE SC_FormsOnline_Forms ALTER COLUMN description varchar(1000);
--- formsOnline/formsOnline-configuration/src/main/config/migrations/db/postgresql/formsOnline/002/create_table.sql
+++ formsOnline/formsOnline-configuration/src/main/config/migrations/db/postgresql/formsOnline/002/create_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE SC_FormsOnline_Forms
 (
 	id integer NOT NULL,
 	xmlFormName character varying(80) NOT NULL,
-	name character varying(80) NOT NULL,
-	description character varying(200),
+	name character varying(200) NOT NULL,
+	description character varying(1000),
 	creationDate date NOT NULL,
 	state integer NOT NULL,
 	instanceId character varying(80) NOT NULL,
