cc32ebe2628f1523a3d521c2887a4358b09b08a5
SilverYoCha <yohann.chastagnier@silverpeas.org>
2018-06-08 16:26:07 +0200

    Bug #9238: fixing the homepage display on 'To validate' folder content access

--- kmelia/kmelia-war/src/main/java/org/silverpeas/components/kmelia/control/KmeliaSessionController.java
+++ kmelia/kmelia-war/src/main/java/org/silverpeas/components/kmelia/control/KmeliaSessionController.java
@@ -669,13 +669,15 @@ public class KmeliaSessionController extends AbstractComponentSessionController
     String nodeId = id;
     if (!StringUtil.isDefined(id)) {
       nodeId = getCurrentFolderId();
-      try {
-        // check that current node still exists
-        getKmeliaBm().getNodeHeader(nodeId, getComponentId());
-      } catch (Exception e) {
-        SilverLogger.getLogger(this).warn(e);
-        setCurrentFolderId(NodePK.ROOT_NODE_ID, true);
-        nodeId = NodePK.ROOT_NODE_ID;
+      if (!KmeliaHelper.isToValidateFolder(nodeId)) {
+        try {
+          // check that current node still exists
+          getKmeliaBm().getNodeHeader(nodeId, getComponentId());
+        } catch (Exception e) {
+          SilverLogger.getLogger(this).warn(e);
+          setCurrentFolderId(NodePK.ROOT_NODE_ID, true);
+          nodeId = NodePK.ROOT_NODE_ID;
+        }
       }
     }
     return getKmeliaBm().getUserTopicProfile(getNodePK(nodeId), getUserId());
