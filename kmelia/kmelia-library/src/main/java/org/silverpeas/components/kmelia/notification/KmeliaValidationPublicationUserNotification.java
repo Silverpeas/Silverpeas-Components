7187b2ae412645704d3828c4cc6071e4f185d160
SilverYoCha <yohann.chastagnier@silverpeas.org>
2018-06-14 16:12:17 +0200

    fixing bug #9701

--- kmelia/kmelia-library/src/main/java/org/silverpeas/components/kmelia/notification/KmeliaValidationPublicationUserNotification.java
+++ kmelia/kmelia-library/src/main/java/org/silverpeas/components/kmelia/notification/KmeliaValidationPublicationUserNotification.java
@@ -81,11 +81,9 @@ public class KmeliaValidationPublicationUserNotification extends AbstractKmeliaA
 
   @Override
   protected Collection<String> getUserIdsToNotify() {
-    final Set<String> userIds = new HashSet<String>();
-    for (String userId : new String[] { getResource().getCreatorId(), getResource().getUpdaterId() }) {
-      if (isDefined(userId)) {
-        userIds.add(userId);
-      }
+    final Set<String> userIds = new HashSet<>();
+    if (isDefined(getResource().getUpdaterId())) {
+      userIds.add(getResource().getUpdaterId());
     }
     return userIds;
   }
