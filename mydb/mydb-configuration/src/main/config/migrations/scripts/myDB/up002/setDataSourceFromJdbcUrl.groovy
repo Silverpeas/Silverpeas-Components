fa2ac43bf00e06e4399ee63225ea0747cf2278bf
Miguel Moquillon <miguel.moquillon@gmail.com>
2018-10-29 12:15:35 +0100

    Update the migration groovy script in MyDB to take into account the context in which an instance of MyDB isn't configured

--- mydb/mydb-configuration/src/main/config/migrations/scripts/myDB/up002/setDataSourceFromJdbcUrl.groovy
+++ mydb/mydb-configuration/src/main/config/migrations/scripts/myDB/up002/setDataSourceFromJdbcUrl.groovy
@@ -30,13 +30,15 @@ import java.sql.SQLException
 
 sql.eachRow('SELECT id, JDBCurl FROM SC_MyDB_ConnectInfo') { row ->
   String jdbcUrl = row.JDBCUrl
-  int idx = jdbcUrl.lastIndexOf('/')
-  String dataSource = "java:/datasources/${jdbcUrl.substring(idx + 1)}"
-  int count = sql.executeUpdate('UPDATE SC_MyDB_ConnectInfo SET dataSource = :dataSource WHERE id = :id',
-      [id: row.id, dataSource: dataSource])
-  if (count != 1) {
-    throw new SQLException("Failed to update connection info of id = ${row.id} with data source = ${dataSource}")
-  } else {
-    log.info "Connection Info of id = ${row.id} -> set data source = ${dataSource}: [Ok]"
+  if (jdbcUrl != null) {
+    int idx = jdbcUrl.lastIndexOf('/')
+    String dataSource = "java:/datasources/${jdbcUrl.substring(idx + 1)}"
+    int count = sql.executeUpdate('UPDATE SC_MyDB_ConnectInfo SET dataSource = :dataSource WHERE id = :id',
+        [id: row.id, dataSource: dataSource])
+    if (count != 1) {
+      throw new SQLException("Failed to update connection info of id = ${row.id} with data source = ${dataSource}")
+    } else {
+      log.info "Connection Info of id = ${row.id} -> set data source = ${dataSource}: [Ok]"
+    }
   }
 }
