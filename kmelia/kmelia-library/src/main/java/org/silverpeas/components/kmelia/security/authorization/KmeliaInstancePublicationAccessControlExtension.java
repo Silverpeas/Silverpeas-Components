557eaa16db92bb4779fea720751613ffaa656cf7
SilverYoCha <yohann.chastagnier@silverpeas.com>
2019-02-22 15:46:30 +0100

    Bug #10415: implementing KmeliaInstancePublicationAccessControlExtension in order to provide specific rules about Kmelia application (same work for kmax and toolbox).

---
+++ kmelia/kmelia-library/src/main/java/org/silverpeas/components/kmelia/security/authorization/KmeliaInstancePublicationAccessControlExtension.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2000 - 2019 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have received a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "https://www.silverpeas.org/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.silverpeas.components.kmelia.security.authorization;
+
+import org.silverpeas.core.security.authorization.DefaultInstancePublicationAccessControlExtension;
+import org.silverpeas.core.util.ResourceLocator;
+import org.silverpeas.core.util.SettingBundle;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * @author silveryocha
+ */
+@Named
+@Singleton
+public class KmeliaInstancePublicationAccessControlExtension
+    extends DefaultInstancePublicationAccessControlExtension {
+
+  private SettingBundle kmeliaSettings = ResourceLocator.getSettingBundle(
+      "org.silverpeas.kmelia.settings.kmeliaSettings");
+
+  @Override
+  protected boolean isDraftVisibleWithCoWriting() {
+    return kmeliaSettings.getBoolean("draftVisibleWithCoWriting", false);
+  }
+}
