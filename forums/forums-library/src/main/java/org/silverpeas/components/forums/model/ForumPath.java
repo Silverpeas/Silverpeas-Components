fa6321f9d44599141c3d0f75229490f65182f0c7
SilverYoCha <yohann.chastagnier@silverpeas.com>
2020-06-19 18:23:50 +0200

    [Almanach Subscription]: continuing the refactoring of the Subscription APIs in order to permits all subscription resource types on personal space.

---
+++ forums/forums-library/src/main/java/org/silverpeas/components/forums/model/ForumPath.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2000 - 2020 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have received a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "https://www.silverpeas.org/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.silverpeas.components.forums.model;
+
+import org.jetbrains.annotations.NotNull;
+import org.silverpeas.core.util.ResourcePath;
+
+import java.util.Collection;
+
+/**
+ * List of {@link Forum} which represents a path.
+ * @author silveryocha
+ */
+public class ForumPath  extends ResourcePath<Forum> {
+  private static final long serialVersionUID = 4607155750736774073L;
+
+  public ForumPath(final @NotNull Collection<Forum> c) {
+    super(c);
+  }
+
+  @Override
+  protected String getInstanceId(final Forum forum) {
+    return forum.getInstanceId();
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected Integer getId(final Forum forum) {
+    return forum.getId();
+  }
+
+  @Override
+  protected boolean isRoot(final Forum forum) {
+    return forum.isRoot();
+  }
+
+  @Override
+  protected String getLabel(final Forum forum, final String language) {
+    return forum.getName();
+  }
+}
---
+++ forums/forums-war/src/main/java/org/silverpeas/components/forums/subscription/bean/ForumSubscriptionBean.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2000 - 2020 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have received a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "https://www.silverpeas.org/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.silverpeas.components.forums.subscription.bean;
+
+import org.silverpeas.components.forums.model.ForumPath;
+import org.silverpeas.components.forums.service.ForumService;
+import org.silverpeas.core.admin.component.model.SilverpeasComponentInstance;
+import org.silverpeas.core.subscription.Subscription;
+import org.silverpeas.core.util.URLUtil;
+import org.silverpeas.core.web.subscription.bean.AbstractSubscriptionBean;
+
+import static org.silverpeas.core.util.URLUtil.getSearchResultURL;
+
+/**
+ * @author silveryocha
+ */
+public class ForumSubscriptionBean extends AbstractSubscriptionBean {
+
+  private final ForumPath path;
+
+  protected ForumSubscriptionBean(final Subscription subscription, final ForumPath path,
+      final SilverpeasComponentInstance component, final String language) {
+    super(subscription, component, language);
+    this.path = path;
+  }
+
+  @Override
+  public String getPath() {
+    return path.format(getLanguage());
+  }
+
+  @Override
+  public String getLink() {
+    return URLUtil.getApplicationURL() +
+        getSearchResultURL(ForumService.get().getForumDetail(path.iterator().next().getPk()));
+  }
+}
