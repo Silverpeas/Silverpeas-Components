0dab7495fe9860f7e65d014149ac66a3eb149cee
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2025-05-22 15:58:38 +0200

    ResourcesManager: update the database creation script for the integration tests which take into account the changes provided by the fix of the bug #14787

--- resourcesManager/resourcesManager-library/src/integration-test/resources/org/silverpeas/components/resourcesmanager/service/create-database.sql
+++ resourcesManager/resourcesManager-library/src/integration-test/resources/org/silverpeas/components/resourcesmanager/service/create-database.sql
@@ -113,6 +113,37 @@ CREATE TABLE ST_Instance_Data
     value       varchar(1000)
 );
 
+CREATE TABLE SB_Node_Node
+(
+    nodeId			 INT		    NOT NULL,
+    nodeName		 VARCHAR (1000)	NOT NULL,
+    nodeDescription	 VARCHAR (2000) NULL,
+    nodeCreationDate VARCHAR (10)	NOT NULL,
+    nodeCreatorId	 VARCHAR (100)	NOT NULL,
+    nodePath		 VARCHAR (1000)	NOT NULL,
+    nodeLevelNumber	 INT		    NOT NULL,
+    nodeFatherId	INT		        NOT NULL,
+    modelId			VARCHAR (1000)	NULL,
+    nodeStatus		VARCHAR (1000)	NULL,
+    instanceId		VARCHAR (50)	NOT NULL,
+    type			VARCHAR (50)	NULL,
+    orderNumber		INT		        DEFAULT (0) NULL,
+    lang			CHAR(2),
+    rightsDependsOn	INT		        DEFAULT (-1) NOT NULL,
+    CONSTRAINT PK_Node_Node PRIMARY KEY (nodeId, instanceId)
+);
+
+CREATE TABLE SB_Node_NodeI18N
+(
+    id			    INT		        NOT NULL,
+    nodeId	        INT	            NOT NULL,
+    lang            CHAR (2)        NOT NULL,
+    nodeName	    VARCHAR (1000)	NOT NULL,
+    nodeDescription	VARCHAR (2000),
+    CONSTRAINT PK_Node_NodeI18N PRIMARY KEY (id),
+    CONSTRAINT UN_Node_NodeI18N UNIQUE (nodeId, lang)
+);
+
 CREATE TABLE SC_Resources_Category
 (
     id            BIGINT      NOT NULL,
